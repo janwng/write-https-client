3260d694afbb337d6ce0a9613eda5347d75ac4bf
Jan Wong <janwong94@gmail.com>
2017-03-13 21:54:51 +0000

    make it into a callback function

---
+++ get-html.js
@@ -0,0 +1,32 @@
+function getHTML(options, callback) {
+
+  var https = require('https');
+
+  https.get(options, function(response) {
+
+    var receivedChunk = '';
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+       receivedChunk += data;
+    });
+
+    response.on('end', function() {
+      printHTML(receivedChunk);
+    });
+  });
+}
+
+function printHTML(html) {
+  console.log(html);
+}
+
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+};
+
+
+// getHTML(requestOptions, printHTML); <- this works too?
+getHTML(requestOptions);
