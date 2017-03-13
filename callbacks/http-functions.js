30b0eaa26b087ff5b294b53ae3989d8f8cfbde6e
Jan Wong <janwong94@gmail.com>
2017-03-13 23:44:24 +0000

    define callback functions

---
+++ callbacks/http-functions.js
@@ -0,0 +1,18 @@
+module.exports = function getHTML(options, callback) {
+
+  var https = require('https');
+
+  https.get(options, function(response) {
+
+    var receivedChunk = '';
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+       receivedChunk += data;
+    });
+
+    response.on('end', function() {
+      callback(receivedChunk);
+    });
+  });
+};
